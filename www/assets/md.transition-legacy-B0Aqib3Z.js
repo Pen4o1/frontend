14ce90341f228574ff313e65a4a5f56fe1046d3e
Pen4o1 <pivv06iwo@gmail.com>
2025-01-15 03:53:03 +0200

    i have build the project for both platforms

--- www/assets/md.transition-legacy-B0Aqib3Z.js
+++ www/assets/md.transition-legacy-B0Aqib3Z.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DBfrynik.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DQa50JYI.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
